d35e3bda95e4739cdd79fd9b56b7aeca7ebdf483
unknown <ezekuiel100@gmail.com>
2025-04-09 17:26:15 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+
+int WINAPI WinMain(
+            HINSTANCE hInstance,
+            HINSTANCE hPrevInstance,    
+            LPSTR     lpCmdLine,
+            int       nShowCmd
+  ){
+    MessageBox(NULL, "This is Handmade Hero", "Handmade hero", MB_OK);
+    return 0;
+  };
