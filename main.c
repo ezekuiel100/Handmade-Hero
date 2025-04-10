c61fb35528289432c2d67ae76c265f248e1f7734
unknown <ezekuiel100@gmail.com>
2025-04-10 17:53:04 -0300

    Main window callback

--- main.c
+++ main.c
@@ -1,5 +1,33 @@
 #include <windows.h>
 
+LRESULT CALLBACK  MainWindowCallback(
+   HWND    hWnd,
+   UINT    Msg,
+   WPARAM  wParam,
+   LPARAM  lParam
+){
+
+  switch (Msg) {
+    case WM_SIZE:
+      OutputDebugStringA("WM_SIZE");
+      break;
+    case WM_DESTROY:
+    OutputDebugStringA("WM_DESTROY\n");
+      break;
+    case WM_CLOSE:
+    OutputDebugStringA("WM_CLOSE\n");
+      break;
+    case WM_ACTIVATEAPP:
+    OutputDebugStringA("WM_ACTIVATEAPP\n");
+      break;
+    default:
+    OutputDebugStringA("Default\n");
+    return DefWindowProc(hWnd, Msg, wParam, lParam);
+  }
+
+  return 0;
+}
+
 
 int WINAPI WinMain(
             HINSTANCE hInstance,
@@ -7,6 +35,16 @@ int WINAPI WinMain(
             LPSTR     lpCmdLine,
             int       nShowCmd
   ){
-    MessageBox(NULL, "This is Handmade Hero", "Handmade hero", MB_OK);
+
+  WNDCLASSA windowsClass = {0};
+
+  windowsClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+  windowsClass.lpfnWndProc = MainWindowCallback;
+  windowsClass.hInstance = hInstance;
+  windowsClass.lpszClassName = "HandmadeHeroWindowClass";
+
+
+  
+
     return 0;
   };
